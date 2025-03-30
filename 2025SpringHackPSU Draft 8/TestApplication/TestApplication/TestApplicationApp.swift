86b58cb1c95ef007a30c063c58b8fd766d898bf8
tslcp <109194998+tslcp@users.noreply.github.com>
2025-03-30 10:55:03 -0400

    Add files via upload

---
+++ 2025SpringHackPSU Draft 8/TestApplication/TestApplication/TestApplicationApp.swift
@@ -0,0 +1,21 @@
+//
+//  TestApplicationApp.swift
+//  TestApplication
+//
+//  Created by Chaoping Li on 3/29/25.
+//
+
+import SwiftUI
+
+@main
+struct TestApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            // For going straight into the AR view
+            // ContentView() (OBSOLETE)
+
+            // For going into the main menu first
+            MainMenuView()
+        }
+    }
+}
