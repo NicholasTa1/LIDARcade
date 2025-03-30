6d535aaeced9e934ef56a4699ec12105fe65ac2c
Kevin Cai <kevincai357@gmail.com>
2025-03-30 02:33:30 -0400

    Add files via upload
    This is where I combined frontend and backend aspects together

---
+++ 2025SpringHackPSU Draft 4.1/TestApplication/TestApplication/MainMenuView.swift
@@ -0,0 +1,43 @@
+//
+//  MainMenuView.swift
+//  LIDARcade
+//
+//  Created by Hannah Wu on 29/3/2025.
+//
+
+import SwiftUI
+import Foundation
+
+struct MainMenuView: View {
+    var body: some View {
+        NavigationView {
+            ZStack {
+                // Background
+                SpaceBackgroundView()
+                
+                VStack(spacing: 40) {
+                    Spacer()
+                    
+                    // Title
+                    ArcadeTitle("LIDARCADE")
+                    
+                    Spacer()
+                    
+                    // Login Buttons
+                    NavigationLink(destination: PatientLoginView()) {
+                        ArcadeButton(text: "PATIENT LOGIN")
+                    }
+                    
+                    NavigationLink(destination: DoctorLoginView()) {
+                        ArcadeButton(text: "DOCTOR LOGIN")
+                    }
+                    
+                    Spacer()
+                }
+                .padding(.bottom, 60)
+            }
+            .navigationBarHidden(true)
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
+    }
+}
